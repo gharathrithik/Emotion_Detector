fe85e115cf299188eafcb57e34d5e88b133d3642
gharathrithik46 <gharatd424@gmail.com>
2025-10-15 08:49:45 -0400

    Final Commit with all the updated tasks

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,6 +7,9 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers = header)
     formatted_response = json.loads(response.text)
+    if response.status_code == 400:
+        return {"anger": None, "disgust": None, "fear": None,
+        "joy": None, "sadness": None, "dominant_emotion": None}
     emotions_dict = formatted_response['emotionPredictions'][0]['emotion']
     dominant_emotion_key = max(emotions_dict, key = emotions_dict.get)
     emotions_dict['dominant_emotion'] = dominant_emotion_key
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+'''
+This is a Module for Emotion Detection in the user provided text using an API
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,13 +9,23 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    '''
+    Function to run the emotion detection API
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     output = emotion_detector(text_to_analyze)
-    return f"For the given statement, the system response is '{output[0]}'. The dominant_emotion is {output[-1]}"
+    if output['dominant_emotion'] is None:
+        return "Invalid text! Please try again!."
+    return f'''For the given statement, the system response is 'anger': {output['anger']}',
+    'disgust': {output['disgust']}, 'fear': {output['fear']}, 'joy': {output['joy']},
+    and 'sadness': {output['sadness']}. The dominant emotion is {output['dominant_emotion']}'''
 
 @app.route("/")
 def render_index_page():
+    '''
+    Function to render the home page
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000)
